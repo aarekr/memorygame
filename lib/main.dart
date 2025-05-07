10d91f129ef9cd6bfee626cfd065f6be7b018689
aarekr <aare212@hotmail.com>
2025-05-07 10:00:25 +0300

    GameScreen

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,23 @@ class StartScreen extends StatelessWidget{
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text("Memory Game", style: TextStyle(fontSize: 30)),
-          ])
+            Padding(padding: EdgeInsets.all(10)),
+            ElevatedButton(
+              child: Text("Start", style: TextStyle(fontSize: 24),),
+              onPressed: () => Get.to(() => GameScreen()),
+            ),
+          ]
+        )
       )
+    );
+  }
+}
+
+class GameScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GameWidget(
+      game: FlameGame(),
     );
   }
 }
