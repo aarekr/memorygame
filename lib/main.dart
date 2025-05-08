5ac55c11ac19cf2d966b92472da52d743cfc92cc
aarekr <aare212@hotmail.com>
2025-05-08 17:42:50 +0300

    ChooseLevelScreen with 3 difficulty levels

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,38 @@ class StartScreen extends StatelessWidget{
             Padding(padding: EdgeInsets.all(10)),
             ElevatedButton(
               child: Text("Start", style: TextStyle(fontSize: 24),),
+              onPressed: () => Get.to(() => ChooseLevelScreen()),
+            ),
+          ]
+        )
+      )
+    );
+  }
+}
+
+class ChooseLevelScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Choose level", style: TextStyle(fontSize: 24)),
+            Padding(padding: EdgeInsets.all(10)),
+            ElevatedButton(
+              child: Text("Beginner 4x4", style: TextStyle(fontSize: 18),),
+              onPressed: () => Get.to(() => GameScreen()),
+            ),
+            Padding(padding: EdgeInsets.all(5)),
+            ElevatedButton(
+              child: Text("Intermediate 5x5", style: TextStyle(fontSize: 18),),
+              onPressed: () => Get.to(() => GameScreen()),
+            ),
+            Padding(padding: EdgeInsets.all(5)),
+            ElevatedButton(
+              child: Text("Advanced 6x6", style: TextStyle(fontSize: 18),),
               onPressed: () => Get.to(() => GameScreen()),
             ),
           ]
